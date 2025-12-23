b19c740bffcd2973f077494e6c7bef2785734e62
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2025-12-24 03:15:59 +0530

    frontend onboard and connected

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,16 @@ app = FastAPI(
     version="1.0"
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # OK for local + college project
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # ------------------------
 # Health Check
